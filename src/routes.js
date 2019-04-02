e35235307e00ff5910496dff854a152dab81c029
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2019-04-02 15:36:59 +0000

    Use browser router

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, HashRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './containers/Home';
 
 export default props => (
-	<HashRouter>
+	<Router>
 		<Switch>
 			<Route exact path="/" component={Home} />
 		</Switch>
-	</HashRouter>
+	</Router>
 );
